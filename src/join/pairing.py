aecfce07d0ec51cc1b18806cddd73a22d19a3730
Andre Meira <andre.meira@deleev.com>
2022-01-16 20:33:21 +0100

    adding pairing classes

--- src/join/pairing.py
+++ src/join/pairing.py
@@ -1,12 +1,14 @@
 from collections import namedtuple
-from typing import Iterable, Callable, Any
+from typing import Iterable, Callable, Any, List
 
 JoinedPair = namedtuple('JoinedPaired', ['a', 'b'])
+JoinedElementToSubset = namedtuple('JoinedElementToSubset', ['element', 'subset'])
 JoinFactory = Callable[[Any, Any], Any]
 JoinCondition = Callable[[Any, Any], bool]
 
 
 class ConditionalPairing:
+    JOIN_THROUGH = JoinedPair
 
     def __init__(
         self,
@@ -19,12 +21,12 @@ class ConditionalPairing:
         self.a: Iterable = a
         self.b: Iterable = b
         self.through: JoinFactory = through\
-            or (lambda a, b: JoinedPair(a, b))
+            or (lambda a, b: self.JOIN_THROUGH(a, b))
 
         self.condition: JoinCondition = cond\
             or (lambda a, b: True)
 
-    def iterate(self):
+    def iterate(self) -> List:
         return [(a, b) for a in self.a for b in self.b]
 
     def join(self) -> Iterable[JoinedPair, Any]:
@@ -42,7 +44,7 @@ class ZipPairing(ConditionalPairing):
     def __init__(self, a: Iterable, b: Iterable, through: JoinFactory = None):
         super().__init__(a, b, through, lambda a, b: True)
 
-    def iterate(self):
+    def iterate(self) -> List:
         b = [item for item in self.b]
         a = [item for item in self.a]
         maxlength = min(len(a), len(b))
@@ -59,4 +61,20 @@ class UnrestrictedPairing(ConditionalPairing):
         super().__init__(a, b, through, lambda a, b: True)
 
 
+class ElementToSubsetPairing(ConditionalPairing):
+    JOIN_THROUGH = JoinedElementToSubset
+    
+    def join(self) -> Iterable[JoinedPair, Any]:
+        return self.iterate()
 
+    def iterate(self):
+        return [
+            self.through(element, self.subset_of(element))
+            for element in self.a
+        ]
+
+    def subset_of(self, element):
+        return [
+            b for b in self.b
+            if self.condition(element, b)
+        ]
